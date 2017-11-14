eadd0f92e875b74c3ac7bdfff27eb6ae80913b75
jouven <jouven@gmail.com>
2017-11-14 20:19:19 +0100

    1 Add some config file functionality 2 Remove the app pointer, Qt already has one 3 Same with QTimer this should be binded to the class that govern each program. 4 remove the "other OUT_TS" function

--- essentialQt.cpp
+++ essentialQt.cpp
@@ -1,23 +1,23 @@
 #include "essentialQt.hpp"
 
-QCoreApplication* qtAppRef_ext(nullptr);
+#include <QFile>
 
 int returnValue_ext = 0;
 
-void qtCout_f(const QString &msg_par_con, const bool addEndl_par_con)
-{
-    static QMutex tmp;
-    tmp.lock();
-    if (addEndl_par_con)
-    {
-        qtOutRef_ext() << msg_par_con << endl;
-    }
-    else
-    {
-        qtOutRef_ext() << msg_par_con;
-    }
-    tmp.unlock();
-}
+//void qtCout_f(const QString &msg_par_con, const bool addEndl_par_con)
+//{
+//    static QMutex tmp;
+//    tmp.lock();
+//    if (addEndl_par_con)
+//    {
+//        qtOutRef_ext() << msg_par_con << endl;
+//    }
+//    else
+//    {
+//        qtOutRef_ext() << msg_par_con;
+//    }
+//    tmp.unlock();
+//}
 
 QTextStream& qtOutRef_ext()
 {
@@ -39,4 +39,84 @@ QMutex& qtOutMutexRef_glo()
     return qoutMutex_glo;
 }
 
-QTimer* qtCycleRef_ext(nullptr);
+//QTimer* qtCycleRef_ext(nullptr);
+
+std::pair<QString, bool> configFile_ext(QString(), false);
+
+void locateConfigFilePath_f(
+        const QCommandLineParser& commandLineParser_par_con
+        , bool checkFirstArgument_par_con)
+{
+    QString configFilePathOrErrorStr;
+    bool configFileFound(false);
+
+    //first argument case
+    if (checkFirstArgument_par_con)
+    {
+        const QStringList parsedPositionalArgs(commandLineParser_par_con.positionalArguments());
+        if (parsedPositionalArgs.size() > 0)
+        {
+            QString configjsonAlternativePathStr(parsedPositionalArgs.at(0));
+            while (true)
+            {
+                if (configjsonAlternativePathStr.isEmpty())
+                {
+                    configFilePathOrErrorStr.append("Config file path is empty");
+                    break;
+                }
+
+                if (not QFile::exists(configjsonAlternativePathStr))
+                {
+                    configFilePathOrErrorStr.append("Config file path doesn't exist " + configjsonAlternativePathStr);
+                    break;
+                }
+                configFilePathOrErrorStr = configjsonAlternativePathStr;
+                configFileFound = true;
+                break;
+            }
+        }
+    }
+
+    //--configFile="somePath" case
+    if (not configFileFound and commandLineParser_par_con.isSet("configFile"))
+    {
+        QString configjsonAlternativePathStr(commandLineParser_par_con.value("configFile"));
+        while (true)
+        {
+            if (configjsonAlternativePathStr.isEmpty())
+            {
+                configFilePathOrErrorStr.append("Config file path is empty");
+                break;
+            }
+
+            if (not QFile::exists(configjsonAlternativePathStr))
+            {
+                configFilePathOrErrorStr.append("Config file path doesn't exist " + configjsonAlternativePathStr);
+                break;
+            }
+            configFilePathOrErrorStr = configjsonAlternativePathStr;
+            configFileFound = true;
+            break;
+        }
+    }
+
+    if (not configFileFound)
+    {
+        QString executablePathConfigFileTmp(QCoreApplication::applicationDirPath() + "/" + QCoreApplication::applicationName() + "_config.json");
+        if (not QFile::exists(executablePathConfigFileTmp))
+        {
+            configFilePathOrErrorStr.append("Config file path doesn't exist " + executablePathConfigFileTmp);
+        }
+        else
+        {
+            configFilePathOrErrorStr = executablePathConfigFileTmp;
+            configFileFound = true;
+        }
+    }
+    configFile_ext = {configFilePathOrErrorStr, configFileFound};
+}
+
+std::pair<QString, bool> configFilePath_f()
+{
+    return configFile_ext;
+}
--- essentialQt.hpp
+++ essentialQt.hpp
@@ -2,27 +2,44 @@
 #ifndef ESSENTIALQTSO_ESSENTIAL_HPP
 #define ESSENTIALQTSO_ESSENTIAL_HPP
 
-#include <QCoreApplication>
+#include <QCommandLineParser>
 #include <QTextStream>
 #include <QMutex>
 #include <QMutexLocker>
-#include <QTimer>
 
+#include <utility>
+
+//use this to use qtOutRef_ext in a thread-safe way
 #define QOUT_TS(X) {QMutexLocker qOutLockerTmp(&qtOutMutexRef_glo()); qtOutRef_ext() << X;}
 
 //Connect the stdout to my qout textstream, not thread-safe
 extern QTextStream &qtOutRef_ext();
-//same as above (and somewhat more limited), but thread-safe
-extern void qtCout_f(const QString& msg_par_con, const bool addEndl_par_con = false);
+////same as above (and somewhat more limited), but thread-safe
+//extern void qtCout_f(const QString& msg_par_con, const bool addEndl_par_con = false);
 //Connect the stderr to my qerr textstream
 extern QTextStream& qtErrRef_ext();
-//so any QObject subclass can reference a parent
-extern QCoreApplication* qtAppRef_ext;
 
 extern int returnValue_ext;
-//necessary for the macro use
+//necessary for the QOUT_TS macro
 extern QMutex& qtOutMutexRef_glo();
 
-extern QTimer* qtCycleRef_ext;
+//extern QTimer* qtCycleRef_ext;
+
+//check for the config file (updates configFile_f):
+//1 if firstArgument_par_con = true, the first argument will be check if it's a file and exists
+//2 if firstArgument_par_con = false, it will check for --configFile="somepath"
+//3 otherwise the path where the program executable is located will be checked for a config.json file
+//it's NOT check if the file is a valid config file, just checks existence
+extern void locateConfigFilePath_f(
+        const QCommandLineParser& commandLineParser_par_con
+        //there is this argument because it can be ambiguous
+        //positional arguments might be used for other stuff that might be paths
+        //but not the config path
+        , bool checkFirstArgument_par_con);
+
+//if the bool is false, QString is an error message
+//else it's the filePath
+//if QString is empty, it hasn't been initialized
+extern std::pair<QString, bool> configFilePath_f();
 
 #endif // ESSENTIALQTSO_ESSENTIAL_HPP
