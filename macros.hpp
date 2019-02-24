1c8ced74c7e85e19e8f1727b8a9760f62eac5cbe
jouven <jouven@gmail.com>
2019-02-24 02:36:57 +0100

    Add a common macros file

--- macros.hpp
+++ macros.hpp
@@ -2,5 +2,13 @@
 #define ESSENTIALQTSO_MACROS_HPP
 
 #define QSTRINGBOOL(BOOL) QString(BOOL ? "true" : "false")
+#define APPENDSTRPTR(STRPTR, MSG) if (STRPTR not_eq nullptr) \
+{ \
+    STRPTR->append(MSG); \
+}
+#define IFERRORSTRPTRNOTEMPTYBREAK(STRPTR) if (not STRPTR->isEmpty()) \
+{ \
+    break; \
+}
 
 #endif // ESSENTIALQTSO_MACROS_HPP
